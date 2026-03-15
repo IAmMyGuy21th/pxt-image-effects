6e8db2187aabc43c5f3bea5659852bc686ef0d98
Hisham <159368595+IAmMyGuy21th@users.noreply.github.com>
2026-03-15 14:18:32 -0700

    Update imageEffects.ts

--- imageEffects.ts
+++ imageEffects.ts
@@ -380,16 +380,15 @@ namespace imageEffects {
     //% mask.shadow=screen_image_picker
     //% group="Effects and Util Reporters"
     export function maskedImage(input: Image, mask: Image, x: number, y: number) {
-        if (mask.width == input.width && mask.height == input.height) {
+        //if (mask.width == input.width && mask.height == input.height) {
             let out = image.create(input.width, input.height)
-            for (let tx = 0; tx < input.width; tx++) {
-                for (let ty = 0; ty < input.height; ty++) {
+            for (let tx = 0; tx < mask.width; tx++) {
+                for (let ty = 0; ty < mask.height; ty++) {
                     out.setPixel(tx+x, ty+y, mask.getPixel(tx, ty) > 0 ? input.getPixel(tx+x, ty+y) : 0)
                 }
             }
             return out
-        }
-        return null
+        //}
     }
 
     /**
@@ -402,13 +401,13 @@ namespace imageEffects {
     //% mask.shadow=screen_image_picker
     //% group="Effects and Util"
     export function setMaskedImage(input: Image, mask: Image, x: number, y: number) {
-        if (mask.width == input.width && mask.height == input.height) {
-            for (let tx = 0; tx < input.width; tx++) {
-                for (let ty = 0; ty < input.height; ty++) {
+        //if (mask.width == input.width && mask.height == input.height) {
+            for (let tx = 0; tx < mask.width; tx++) {
+                for (let ty = 0; ty < mask.height; ty++) {
                     input.setPixel(tx+x, ty+y, mask.getPixel(tx, ty) > 0 ? input.getPixel(tx+x, ty+y) : 0)
                 }
             }
-        }
+        //}
     }
 }
 
